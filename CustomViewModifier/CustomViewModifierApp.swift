363475eb08fc1f5b057a0d4244459c12b1f59c4a
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2021-09-01 21:34:49 +0300

    Initial Commit

---
+++ CustomViewModifier/CustomViewModifierApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomViewModifierApp.swift
+//  CustomViewModifier
+//
+//  Created by ramil on 01.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct CustomViewModifierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
